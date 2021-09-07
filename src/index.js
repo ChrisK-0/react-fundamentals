c5bbdfcd98bf0e6c5d470dab20a61a65d7418717
ChrisK-0 <73487474+ChrisK-0@users.noreply.github.com>
2021-09-07 18:15:33 +0300

    Initial

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const getCurrentDate = () => {
+  const date = new Date()
+  return date.toDateString()
+}
+const greeting = <h1>Hello there. Current date: {getCurrentDate()} </h1>
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  greeting,
   document.getElementById('root')
 );
 
